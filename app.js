907bae71f82c26e81a65f80c2a4eff3d4d87baba
tarantino19 <tolentinored19@gmail.com>
2023-09-23 17:13:00 +0800

    add static file

--- app.js
+++ app.js
@@ -5,7 +5,12 @@ const moviesRouter = require ('./Routes/moviesRoutes')
 
 app.use(express.json())
 app.use(morgan('dev')) 
-
+app.use(express.static('./public'))
 app.use('/api/v1/movies', moviesRouter) 
 
 module.exports = app;
+
+
+//sending an html file - e.g. 404 error , a page or something
+// app. get('/', (req, res) => {
+//   res. sendFile(path. join(__dirname, 'index.html'));
